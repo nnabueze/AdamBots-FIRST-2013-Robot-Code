f69851255a0b5ea35bd7d6725507a7118decb167
SimplePay1 <SimplePay1@SimplePay1-HP>
2016-03-04 13:39:54 +0100

    added test file

--- testing.php
+++ testing.php
@@ -9,12 +9,30 @@
 class testing {
     
     protected $name;
+    public $age;
     
+    /*
+     * @dec set the name of the person when the class being intanciated
+     * @param string $name
+     * @return void
+     */
     public function __construct(String $name) {
         $this->name = $name;
     }
-    
+    /*
+     * @dec get the name of the individual
+     */
     public function getName(){
         return $this->name;
+    }
+    /*
+     * @Dec set the age of the individual and prints it out.
+     * @param string age
+     * @return void
+     */
+    public function setAge(int $age){
+        $this->age = $age;
+        echo $this->age;
+        
     }
 }
